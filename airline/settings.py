2ba97361c6822daa808dc8ebc27f2dba60fc3f7e
kamissah <kwameamissah@yahoo.com>
2019-12-05 08:26:45 +0000

    changes to email setup in airline/settings.py

--- airline/settings.py
+++ airline/settings.py
@@ -130,8 +130,8 @@ MEDIA_URL = '/files/'
 # email client
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'kamissah.gh@gmail.com'
-EMAIL_HOST_PASSWORD = 'P@s$p0rt'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'emailpassword'
 EMAIL_PORT = 587
 
 ''' End Self added '''
